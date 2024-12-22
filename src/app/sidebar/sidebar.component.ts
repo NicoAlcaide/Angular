ecbed68d2a49b78a085218513f73b774e8a79953
NicoAlcaide <nicolas.alcaide714@comunidadunir.net>
2024-12-22 19:17:16 +0100

    Sidebar mejorado + logos

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
 import { CommonModule } from '@angular/common';
-import { MatListModule } from '@angular/material/list';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule para ngModel
+import { MatExpansionModule } from '@angular/material/expansion'; // Importa MatExpansionModule
+import { MatListModule } from '@angular/material/list'; 
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, MatListModule, MatIconModule],
+  imports: [CommonModule, MatListModule, MatIconModule, MatExpansionModule, FormsModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
