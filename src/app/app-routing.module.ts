665ef0ee88039c89895b17d25e17d1d33145f923
NicoAlcaide <nicolas.alcaide714@comunidadunir.net>
2024-12-27 08:22:28 +0100

    limpia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,16 @@ import { RouterModule, Routes } from '@angular/router';
 //import { HelloWorldComponent } from './hello-world/hello-world.component';
 // import { ProgrammingComponent } from './programming/programming.component';
 //import { ConclusionsComponent } from './conclusions/conclusions.component';
-import { DniCalculatorComponent } from './dni-calculator/dni-calculator.component';
-import { ResultPageComponent } from './result-page/result-page.component';
+//import { DniCalculatorComponent } from './dni-calculator/dni-calculator.component';
+//import { ResultPageComponent } from './result-page/result-page.component';
 
-const routes: Routes = [
- 
-  { path: 'dni-calculator', loadComponent: () => import('./dni-calculator/dni-calculator.component').then(m => m.DniCalculatorComponent) },
-  { path: 'result-page', loadComponent: () => import('./result-page/result-page.component').then(m => m.ResultPageComponent) },
-  { path: '', redirectTo: '/introduction', pathMatch: 'full' }
-];
+
+// { path: 'dni-calculator', loadComponent: () => import('./dni-calculator/dni-calculator.component').then(m => m.DniCalculatorComponent) },
+//  { path: 'result-page', loadComponent: () => import('./result-page/result-page.component').then(m => m.ResultPageComponent) },
+//  { path: '', redirectTo: '/introduction', pathMatch: 'full' }
+  
+  
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
